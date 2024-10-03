afdb3fbeb720e3421d2e8d7a8e1d5e0f3dbbe7ca
suraj <s373211@oslomet.no>
2024-10-03 15:05:41 +0200

    modified in project sturcture

--- template.py
+++ template.py
@@ -4,28 +4,43 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]:%(message)s')
 
-model_name = "detectron2"
+model_name1 = "detectron2"
 model_name2 = "faster_rcnn"
+model_name3 = "yolov8"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
-    f"src/{model_name}/__init__.py",
-    f"src/{model_name}/components/__init__.py",
-    f"src/{model_name}/utils/__init__.py",
-    f"src/{model_name}/config/__init__.py",
-    f"src/{model_name}/config/configuration.py",
-    f"src/{model_name}/pipeline/__init__.py",
-    f"src/{model_name}/entity/__init__.py",
-    f"src/{model_name}/constants/__init__.py",
-
-    f"src/{model_name2}/__init__.py",
-    f"src/{model_name2}/components/__init__.py",
-    f"src/{model_name2}/utils/__init__.py",
-    f"src/{model_name2}/config/__init__.py",
-    f"src/{model_name2}/config/configuration.py",
-    f"src/{model_name2}/pipeline/__init__.py",
-    f"src/{model_name2}/entity/__init__.py",
-    f"src/{model_name2}/constants/__init__.py",
+    "docs/STRUCTURE.md",
+    f"src/modules/ballot_creation/__init__.py",
+    f"src/modules/symbol_detection/{model_name1}/__init__.py",
+    f"src/modules/symbol_detection/{model_name1}/components/__init__.py",
+    f"src/modules/symbol_detection/{model_name1}/utils/__init__.py",
+    f"src/modules/symbol_detection/{model_name1}/config/__init__.py",
+    f"src/modules/symbol_detection/{model_name1}/config/configuration.py",
+    f"src/modules/symbol_detection/{model_name1}/pipeline/__init__.py",
+    f"src/modules/symbol_detection/{model_name1}/entity/__init__.py",
+    f"src/modules/symbol_detection/{model_name1}/constants/__init__.py",
+    f"src/modules/vote_validation/{model_name1}/__init__.py",
+    
+    f"src/modules/symbol_detection/{model_name2}/__init__.py",
+    f"src/modules/symbol_detection/{model_name2}/components/__init__.py",
+    f"src/modules/symbol_detection/{model_name2}/utils/__init__.py",
+    f"src/modules/symbol_detection/{model_name2}/config/__init__.py",
+    f"src/modules/symbol_detection/{model_name2}/config/configuration.py",
+    f"src/modules/symbol_detection/{model_name2}/pipeline/__init__.py",
+    f"src/modules/symbol_detection/{model_name2}/entity/__init__.py",
+    f"src/modules/symbol_detection/{model_name2}/constants/__init__.py",
+    f"src/modules/vote_validation/{model_name2}/__init__.py",
+  
+    f"src/modules/symbol_detection/{model_name3}/__init__.py",
+    f"src/modules/symbol_detection/{model_name3}/components/__init__.py",
+    f"src/modules/symbol_detection/{model_name3}/utils/__init__.py",
+    f"src/modules/symbol_detection/{model_name3}/config/__init__.py",
+    f"src/modules/symbol_detection/{model_name3}/config/configuration.py",
+    f"src/modules/symbol_detection/{model_name3}/pipeline/__init__.py",
+    f"src/modules/symbol_detection/{model_name3}/entity/__init__.py",
+    f"src/modules/symbol_detection/{model_name3}/constants/__init__.py",
+    f"src/modules/vote_validation/{model_name3}/__init__.py",
 
     "config/config.yaml",
     "dvc.yaml",
