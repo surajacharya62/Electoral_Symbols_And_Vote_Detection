2f16ccc05beba4c25635a1fc20d52c3b01d70d24
suraj <s373211@oslomet.no>
2024-11-10 18:40:16 +0100

    added data ingestion and pipeline

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from modules import logger
+from modules.symbol_detection.faster_rcnn.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("welcome to electorals and sysmbols detection project")
+# logger.info("welcome to electorals and sysmbols detection project")
+
+STAGE_NAME = "Data Ingestion Stage"
+
+
+try:
+    logger.info(f">>>>>>> Stage {STAGE_NAME} started <<<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>> Stage {STAGE_NAME} completed <<<<<<<\nx==================x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
